5593a742a0069e8d7e39833847b37916c3a8e9b2
Fang <kevin7109g@gmail.com>
2024-01-18 10:01:56 +0800

    feat: simplify PasswordVM transform function

--- PasswordCombine/PasswordCombine/ViewModel/PasswordViewModel.swift
+++ PasswordCombine/PasswordCombine/ViewModel/PasswordViewModel.swift
@@ -27,44 +27,55 @@ class PasswordViewModel {
         
         let updatePublisher = input.newPasswordPublisher.flatMap { [unowned self] newPassword in
             
-            print("newPassword: \(newPassword)")
-            // Empty text
-            guard  !newPassword.isEmpty else {
-                return Just(Result(validate: false, message: "Enter your password", text: newPassword)).eraseToAnyPublisher()
-            }
+            return self.newPasswordValidation(text: newPassword)
             
-            // Valid characters
-            let validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,@:?!()$\\/#"
-            let invalidSet = CharacterSet(charactersIn: validChars).inverted
-            guard newPassword.rangeOfCharacter(from: invalidSet) == nil else {
-                return Just(Result(validate: false, message: "Enter valid special chars (.,@:?!()$\\/#) with no spaces", text: newPassword)).eraseToAnyPublisher()
-            }
-            
-            // Criteria met
-            if !self.validate(newPassword) {
-                return Just(Result(validate: false, message: "Your password must meet the requirements below", text: newPassword)).eraseToAnyPublisher()
-            }
-            
-            return Just(Result(validate: true, message: "", text: newPassword)).eraseToAnyPublisher()
         }.eraseToAnyPublisher()
         
-        let resultPublisher = Publishers.CombineLatest(input.newPasswordPublisher, input.confirmPasswordPublisher).flatMap {   (newPassword, confirmPassword) in
+        let resultPublisher = Publishers.CombineLatest(input.newPasswordPublisher, input.confirmPasswordPublisher).flatMap {  [unowned self] (newPassword, confirmPassword) in
             
-            guard !confirmPassword.isEmpty else {
-                return Just(Result(validate: false, message: "Enter your password", text: confirmPassword)).eraseToAnyPublisher()
-            }
+            return self.confrimPasswordValidation(newPassword: newPassword, confirmPassword: confirmPassword)
             
-            guard confirmPassword == newPassword else {
-                return Just(Result(validate: false, message: "Password do not match.", text: confirmPassword)).eraseToAnyPublisher()
-            }
-            
-            return Just(Result(validate: true, message: "", text: "")).eraseToAnyPublisher()
         }.eraseToAnyPublisher()
         
         return Output(updatePublisher: updatePublisher, resultPublisher: resultPublisher)
     }
     
-    func validate(_ text: String) -> Bool {
+    private func newPasswordValidation(text newPassword: String) -> AnyPublisher<Result, Never> {
+        
+        // Empty text
+        guard  !newPassword.isEmpty else {
+            return Just(Result(validate: false, message: "Enter your password", text: newPassword)).eraseToAnyPublisher()
+        }
+        
+        // Valid characters
+        let validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,@:?!()$\\/#"
+        let invalidSet = CharacterSet(charactersIn: validChars).inverted
+        guard newPassword.rangeOfCharacter(from: invalidSet) == nil else {
+            return Just(Result(validate: false, message: "Enter valid special chars (.,@:?!()$\\/#) with no spaces", text: newPassword)).eraseToAnyPublisher()
+        }
+        
+        // Criteria met
+        if !self.validate(newPassword) {
+            return Just(Result(validate: false, message: "Your password must meet the requirements below", text: newPassword)).eraseToAnyPublisher()
+        }
+        
+        return Just(Result(validate: true, message: "", text: newPassword)).eraseToAnyPublisher()
+    }
+    
+    private func confrimPasswordValidation(newPassword: String, confirmPassword: String) -> AnyPublisher<Result, Never> {
+        
+        guard !confirmPassword.isEmpty else {
+            return Just(Result(validate: false, message: "Enter your password", text: confirmPassword)).eraseToAnyPublisher()
+        }
+        
+        guard confirmPassword == newPassword else {
+            return Just(Result(validate: false, message: "Password do not match.", text: confirmPassword)).eraseToAnyPublisher()
+        }
+        
+        return Just(Result(validate: true, message: "", text: "")).eraseToAnyPublisher()
+    }
+    
+    private func validate(_ text: String) -> Bool {
         let uppercaseMet = PasswordCriteria.uppercaseMet(text)
         let lowercaseMet = PasswordCriteria.lowercaseMet(text)
         let digitMet = PasswordCriteria.digitMet(text)
