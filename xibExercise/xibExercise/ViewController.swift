dbc24bb59377d7f21a1628e864f2bf4abc8f75f8
Fang <kevin7109g@gmail.com>
2024-02-21 10:48:14 +0800

    initial commit

---
+++ xibExercise/xibExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xibExercise
+//
+//  Created by 方奎元 on 2024/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
